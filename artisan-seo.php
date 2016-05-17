9b972bade400f0b8cd69d985a616c592fc23c969
Jeff Kilroy <jeffkilroy@gmail.com>
2016-05-17 14:37:00 -0400

    Version update

--- artisan-seo.php
+++ artisan-seo.php
@@ -3,7 +3,7 @@
  * Plugin Name: ArtisanSEO
  * Plugin URI: http://artisanseo.com
  * Description: Pull ArtisanSEO Templates
- * Version: 0.1.3
+ * Version: 0.1.4
  * Author: Kilroy Web Development
  * Author URI: http://kilroyweb.com
  * License: GPL2
